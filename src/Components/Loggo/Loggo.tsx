c2cb97a315b8403a07a26487fa88df49bc256c50
Lars Abelsson <georg483@msn.com>
2024-03-22 20:16:52 +0100

    Header Loggo

---
+++ src/Components/Loggo/Loggo.tsx
@@ -0,0 +1,12 @@
+const Loggo = () => {
+  return (
+    <div className="loggo">
+      <div>
+        <p>Frontend</p>
+        <p>Pizza</p>
+      </div>
+    </div>
+  )
+}
+
+export default Loggo
--- src/Pages/Menu.tsx
+++ src/Pages/Menu.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react"
+import Header from "../Components/Header/Header"
 
 const Menu = () => {
   return (
-    <div>
+    <>
+      <Header />
+      <main>
         <h2>Menu</h2>
-      
-    </div>
+      </main>
+    </>
   )
 }
 
