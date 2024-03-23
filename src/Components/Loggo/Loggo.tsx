3b3991851881045b6b247a1237c85b1c3fb1ad4a
Lars Abelsson <georg483@msn.com>
2024-03-23 16:34:28 +0100

    Moved header component to app.tsx and put a link to start page in Loggo component

--- src/Components/Loggo/Loggo.tsx
+++ src/Components/Loggo/Loggo.tsx
@@ -1,11 +1,11 @@
 const Loggo = () => {
   return (
-    <div className="loggo">
+    <a href="./" className="loggo">
       <div>
         <p>Frontend</p>
         <p>Pizza</p>
       </div>
-    </div>
+    </a>
   )
 }
 
--- src/Pages/Menu.tsx
+++ src/Pages/Menu.tsx
@@ -1,10 +1,6 @@
-import React from "react"
-import Header from "../Components/Header/Header"
-
 const Menu = () => {
   return (
     <>
-      <Header />
       <main>
         <h2>Menu</h2>
       </main>
