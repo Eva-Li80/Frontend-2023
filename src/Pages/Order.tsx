de10aedc3e1e2d30db153a30b1ca3c6ff712579b
Eva-Li80 <evaliboden@gmail.com>
2024-03-24 12:27:23 +0100

    styling start

--- src/Pages/Order.tsx
+++ src/Pages/Order.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import "./order-style.scss"
 
 const Order = () => {
   return (
     <div>
-        <h2>Order page</h2>
-      
+        <h2 className='order'>Order page</h2>
     </div>
   )
 }
