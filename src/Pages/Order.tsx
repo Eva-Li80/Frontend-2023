b2232f13dd1311f0d5e3b87c118b256c5432ed26
Eva-Li80 <evaliboden@gmail.com>
2024-03-25 09:57:20 +0100

    mui

--- src/Pages/Order.tsx
+++ src/Pages/Order.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import "./order-style.scss"
+
+import Button from '@mui/material/Button';
 
 const Order = () => {
   return (
     <div>
         <h2 className='order'>Order page</h2>
+        <Button variant="contained">Contained</Button>
     </div>
   )
 }
