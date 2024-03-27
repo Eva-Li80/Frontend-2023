5e6ba6add1402b07d679a6f840d38e89e832325e
Eva-Li80 <evaliboden@gmail.com>
2024-03-27 20:47:26 +0100

    style and clean code

---
+++ src/Components/PizzaOrder/PizzaOrder.tsx
@@ -0,0 +1,95 @@
+import { useContext } from "react";
+import { PizzaContext } from "../../context/PizzaContextProvider";
+import { CartItem } from "../../Type";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import PizzaOrdered from "./PizzaOrdered";
+import PizzaOrderListItem from "./PizzaOrderListItem";
+import Loggo from "../Loggo/Loggo";
+
+const PizzaOrder = () => {
+  const { state, dispatch } = useContext(PizzaContext);
+
+  const handelUpdateQuantity = (cart: CartItem, updateQuantitiy: string) => {
+    if (updateQuantitiy === "+") {
+      dispatch({
+        type: "UpdateCartItem",
+        payload: {
+          ...cart,
+          quantity: cart.quantity + 1,
+        },
+      });
+    } else if (updateQuantitiy === "-") {
+      if (cart.quantity >= 1) {
+        dispatch({
+          type: "UpdateCartItem",
+          payload: {
+            ...cart,
+            quantity: cart.quantity - 1,
+          },
+        });
+      } else {
+        dispatch({
+          type: "RemoveCartItem",
+          payload: cart.id,
+        });
+      }
+    }
+  };
+
+  const handleDeleteCartItem = (id: string) => {
+    dispatch({
+      type: "RemoveCartItem",
+      payload: id,
+    });
+  };
+
+  const calculateTotalPrice = () => {
+    return state.cart
+      .reduce((total, pizza) => total + pizza.product.price * pizza.quantity, 0)
+      .toFixed(2);
+  };
+
+  return (
+    <>
+      <div className="pizza-container">
+        <div className="order-header-container">
+          <Loggo />
+          <h2 className="order-header">Din beställning!</h2>
+        </div>
+        <div>
+          {state.cart.map((pizza) => (
+            <PizzaOrderListItem
+              pizza={pizza}
+              handleUpdateQuantity={handelUpdateQuantity}
+              handleDelete={handleDeleteCartItem}
+            />
+          ))}
+
+          {state.cart && state.cart.length > 0 ? (
+            <h3 className="price-cartitem">
+              Totalt pris: {calculateTotalPrice()} Kr
+            </h3>
+          ) : (
+            <div className="no-order">
+              <h3 className="price-cartitem text">
+                Ingen order : {calculateTotalPrice()} Kr
+              </h3>
+            </div>
+          )}
+        </div>
+        <div className="order-btn-container">
+          <Button className="btn" variant="contained">
+            <Link className="link" to="/">
+              {" "}
+              Lägg till mer ?
+            </Link>
+          </Button>
+          <PizzaOrdered />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PizzaOrder;
--- src/Components/PizzaOrder/PizzaOrdered.tsx
+++ src/Components/PizzaOrder/PizzaOrdered.tsx
@@ -10,31 +10,39 @@ import {
 import { PizzaContext } from "../../context/PizzaContextProvider";
 
 const PizzaOrdered = () => {
- const { state, dispatch } = useContext(PizzaContext)
+  const { state, dispatch } = useContext(PizzaContext);
   const [open, setOpen] = useState(false);
 
   const handleClickToggle = () => {
-    setOpen(!open)
-    dispatch({type: "ClearCart"})
-  }
+    setOpen(!open);
+    dispatch({ type: "ClearCart" });
+  };
 
   return (
     <div>
-      <Button variant="contained" onClick={handleClickToggle} className="btn"
-      disabled={state.cart.length === 0}
+      <Button
+        variant="contained"
+        onClick={handleClickToggle}
+        className="btn"
+        disabled={state.cart.length === 0}
       >
         Beställ
       </Button>
       <Dialog open={open}>
         <div className="dialog">
           <DialogTitle>Tack för beställningen!</DialogTitle>
-          <DialogContent>
+          <DialogContent className="dialog-content">
             <Typography variant="body1">
               Din beställning har mottagits.
             </Typography>
+            <Typography>
+              Välkommen åter!
+            </Typography>
           </DialogContent>
-          <DialogActions >
-            <Button className="action-btn" onClick={handleClickToggle}>X</Button>
+          <DialogActions>
+            <Button className="action-btn" onClick={handleClickToggle}>
+              X
+            </Button>
           </DialogActions>
         </div>
       </Dialog>
